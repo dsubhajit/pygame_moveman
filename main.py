4cc0e6fa8822344dd568b30b1ee7d327d7848292
Rahul Ghose <hansum.rahul@gmail.com>
2015-03-26 22:42:33 +0530

    loaded image into sprite. first step

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from pygame.locals import *
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
+from helper import *
+
+class Hero(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('man.png',-1)
+		self.positionx = 10
+		self.positiony = 10
+
 class PyManMain:
 	def __init__(self, width=640,height=480):
 		pygame.init()
@@ -12,8 +21,15 @@ class PyManMain:
 		self.height = height
 		self.screen = pygame.display.set_mode((self.width, self.height))
 
+	def LoadSprites(self):
+		self.hero = Hero()
+		self.hero_sprites = pygame.sprite.RenderPlain((self.hero))
+
 	def MainLoop(self):
+		self.LoadSprites()
 		while 1:
+			self.hero_sprites.draw(self.screen)
+			pygame.display.flip()
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT: 
 					sys.exit()

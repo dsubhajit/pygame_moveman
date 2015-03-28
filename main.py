95f4dc491f95e6bb4e499c2c32103f4fd7e4c311
Rahul Ghose <hansum.rahul@gmail.com>
2015-03-28 22:50:37 +0530

    the character can now move in peace. updated ignore file as wel

--- main.py
+++ main.py
@@ -11,8 +11,24 @@ class Hero(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 		self.image, self.rect = load_image('man.png',-1)
-		self.positionx = 10
-		self.positiony = 10
+		# Pixels to move each time
+		self.x_dist = 5
+		self.y_dist = 5
+	
+	def move(self,key):
+		xMove = 0;
+		yMove = 0;
+		if (key == K_RIGHT):
+			xMove = self.x_dist
+		elif (key == K_LEFT):
+			xMove = -self.x_dist
+		elif (key == K_UP):
+			yMove = -self.y_dist
+		elif (key == K_DOWN):
+			yMove = self.y_dist
+		
+		self.rect.move_ip(xMove,yMove);
+
 
 class PyManMain:
 	def __init__(self, width=640,height=480):
@@ -24,15 +40,52 @@ class PyManMain:
 	def LoadSprites(self):
 		self.hero = Hero()
 		self.hero_sprites = pygame.sprite.RenderPlain((self.hero))
-
+	
 	def MainLoop(self):
+		pygame.key.set_repeat(500, 30)
+		clock = pygame.time.Clock()
 		self.LoadSprites()
+
 		while 1:
-			self.hero_sprites.draw(self.screen)
+			clock.tick(50)
+			keystates={'up':False, 'down':False, 'left':False, 'right':False}
 			pygame.display.flip()
+			self.hero_sprites.draw(self.screen)
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT: 
 					sys.exit()
+				elif event.type == pygame.KEYDOWN:
+					if event.key == K_UP:
+						keystates['up']=True
+					elif event.key == K_DOWN:
+						keystates['down'] = True
+					elif event.key == K_LEFT:
+						keystates['left'] = True
+					elif event.key == K_RIGHT:
+						keystates['right'] = True
+						
+			#keys = pygame.key.get_pressed()
+			#if keys[K_LEFT]:
+			#	keystates['left'] = True
+			#if keys[K_RIGHT]:
+			#	keystates['right'] = True
+			#if keys[K_UP]:
+			#	keystates['up'] = True
+			#if keys[K_DOWN]:
+			#	keystates['down'] = True
+
+			if keystates['up'] == True:
+				self.screen.fill((0,0,0))
+				self.hero.move(K_UP)
+			if keystates['down'] == True:
+				self.screen.fill((0,0,0))
+				self.hero.move(K_DOWN)
+			if keystates['left'] == True:
+				self.screen.fill((0,0,0))
+				self.hero.move(K_LEFT)
+			if keystates['right'] == True:
+				self.screen.fill((0,0,0))
+				self.hero.move(K_RIGHT)
 
 if __name__ == "__main__":
 	MainWindow = PyManMain()
